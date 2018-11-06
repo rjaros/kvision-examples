b594ee541dcf288f9bb98a66fd978dc9e5537001
Robert Jaros <rjaros@finn.pl>
2018-11-06 14:38:23 +0100

    Color selection

--- desktop/src/main/kotlin/com/example/Paint.kt
+++ desktop/src/main/kotlin/com/example/Paint.kt
@@ -5,6 +5,7 @@
 package com.example
 
 import org.w3c.dom.CanvasRenderingContext2D
+import pl.treksoft.kvision.core.Background
 import pl.treksoft.kvision.core.Border
 import pl.treksoft.kvision.core.BorderStyle
 import pl.treksoft.kvision.core.Col
@@ -12,15 +13,18 @@ import pl.treksoft.kvision.core.Container
 import pl.treksoft.kvision.html.Button
 import pl.treksoft.kvision.html.Button.Companion.button
 import pl.treksoft.kvision.html.Canvas
+import pl.treksoft.kvision.html.Icon.Companion.icon
 import pl.treksoft.kvision.panel.DockPanel.Companion.dockPanel
 import pl.treksoft.kvision.panel.FlexAlignItems
-import pl.treksoft.kvision.panel.HPanel
+import pl.treksoft.kvision.panel.HPanel.Companion.hPanel
 import pl.treksoft.kvision.panel.Side
 import pl.treksoft.kvision.panel.VPanel
 import pl.treksoft.kvision.utils.perc
 import pl.treksoft.kvision.utils.px
 import kotlin.math.PI
 import kotlin.math.abs
+
+val colors = arrayOf(Col.WHITE, Col.BLACK, Col.RED, Col.GREEN, Col.BLUE, Col.YELLOW)
 
 class Paint : DesktopWindow("Paint", 600, 400) {
 
@@ -29,6 +33,9 @@ class Paint : DesktopWindow("Paint", 600, 400) {
     lateinit var buttonLine: Button
     lateinit var buttonRectangle: Button
     lateinit var buttonCircle: Button
+
+    var lineColorButtons = mutableListOf<Button>()
+    var fillColorButtons = mutableListOf<Button>()
 
     init {
         dockPanel {
@@ -53,7 +60,6 @@ class Paint : DesktopWindow("Paint", 600, 400) {
                         canvas.selectedTool = Tool.PENCIL
                         selectTool(this)
                     }
-                    this.border = Border(1.px, BorderStyle.SOLID, Col.BLUE)
                 }
                 buttonLine = button("", "fa-minus") {
                     title = "Line"
@@ -76,11 +82,68 @@ class Paint : DesktopWindow("Paint", 600, 400) {
                         selectTool(this)
                     }
                 }
+                button("", "fa-undo") {
+                    title = "Undo"
+                    marginTop = 20.px
+                    onClick {
+                        canvas.undo()
+                    }
+                }
             }
             add(buttons, Side.LEFT)
-            val colors = HPanel {
+            val colors = VPanel(spacing = 8) {
+                paddingLeft = 80.px
                 height = 60.px
                 width = 100.perc
+
+                hPanel(spacing = 6) {
+                    icon("fa-heart-o") {
+                        title = "Line color"
+                        marginRight = 4.px
+                    }
+                    colors.forEach { color ->
+                        val b = button("") {
+                            width = 32.px
+                            height = 22.px
+                            background = Background(color = color)
+                            onClick {
+                                canvas.borderColor = color
+                                selectLineColor(this)
+                            }
+                        }
+                        lineColorButtons.add(b)
+                    }
+                }
+
+                hPanel(spacing = 6) {
+                    icon("fa-heart") {
+                        title = "Fill color"
+                        marginRight = 4.px
+                    }
+                    colors.forEach { color ->
+                        val b = button("") {
+                            width = 32.px
+                            height = 22.px
+                            background = Background(color = color)
+                            onClick {
+                                canvas.fillColor = color
+                                selectFillColor(this)
+                            }
+                        }
+                        fillColorButtons.add(b)
+                    }
+                    val b = button("", classes = setOf("stripes")) {
+                        title = "Empty fill"
+                        width = 32.px
+                        height = 22.px
+                        onClick {
+                            canvas.fillColor = null
+                            selectFillColor(this)
+                        }
+                    }
+                    fillColorButtons.add(b)
+                }
+
             }
             add(colors, Side.DOWN)
             this@Paint.setEventListener<Paint> {
@@ -91,6 +154,9 @@ class Paint : DesktopWindow("Paint", 600, 400) {
                 }
             }
         }
+        selectTool(buttonPencil)
+        selectLineColor(lineColorButtons[1])
+        selectFillColor(fillColorButtons[0])
     }
 
     fun selectTool(button: Button) {
@@ -99,6 +165,16 @@ class Paint : DesktopWindow("Paint", 600, 400) {
         buttonLine.border = null
         buttonRectangle.border = null
         buttonCircle.border = null
+        button.border = Border(1.px, BorderStyle.SOLID, Col.BLUE)
+    }
+
+    fun selectLineColor(button: Button) {
+        lineColorButtons.forEach { it.border = null }
+        button.border = Border(1.px, BorderStyle.SOLID, Col.BLUE)
+    }
+
+    fun selectFillColor(button: Button) {
+        fillColorButtons.forEach { it.border = null }
         button.border = Border(1.px, BorderStyle.SOLID, Col.BLUE)
     }
 
@@ -112,6 +188,9 @@ class Paint : DesktopWindow("Paint", 600, 400) {
 class PaintCanvas(width: Int, height: Int) : Canvas(width, height) {
 
     var selectedTool = Tool.PENCIL
+    var borderColor = Col.BLACK
+    var fillColor: Col? = Col.WHITE
+
     private var drawing = mutableListOf<Fig>()
     private var currentFig: Fig? = null
 
@@ -131,25 +210,33 @@ class PaintCanvas(width: Int, height: Int) : Canvas(width, height) {
         }
     }
 
+    fun undo() {
+        if (drawing.size > 0) {
+            drawing.removeAt(drawing.size - 1)
+            redraw()
+        }
+    }
+
     private fun handleMouseDown(x: Int, y: Int) {
         when (selectedTool) {
             Tool.POINT -> {
-                currentFig = Point(x, y)
-                context2D.fillRect(x.toDouble(), y.toDouble(), 1.0, 1.0)
+                currentFig = Point(x, y, borderColor)
+                context2D.strokeStyle = borderColor.name
+                context2D.strokeRect(x.toDouble(), y.toDouble(), 1.0, 1.0)
             }
             Tool.PENCIL -> {
-                currentFig = Pencil(x, y)
+                currentFig = Pencil(x, y, borderColor)
                 context2D.beginPath()
                 context2D.moveTo(x.toDouble(), y.toDouble())
             }
             Tool.LINE -> {
-                currentFig = Line(x, y)
+                currentFig = Line(x, y, borderColor)
             }
             Tool.RECTANGLE -> {
-                currentFig = Rectangle(x, y)
+                currentFig = Rectangle(x, y, borderColor, fillColor)
             }
             Tool.CIRCLE -> {
-                currentFig = Circle(x, y)
+                currentFig = Circle(x, y, borderColor, fillColor)
             }
         }
     }
@@ -157,6 +244,7 @@ class PaintCanvas(width: Int, height: Int) : Canvas(width, height) {
     private fun handleMouseMove(x: Int, y: Int, fig: Fig) {
         when (fig) {
             is Pencil -> {
+                context2D.strokeStyle = borderColor.name
                 context2D.lineTo(x.toDouble(), y.toDouble())
                 context2D.stroke()
                 fig.addPoint(x, y)
@@ -164,6 +252,7 @@ class PaintCanvas(width: Int, height: Int) : Canvas(width, height) {
             is Line -> {
                 redraw()
                 context2D.beginPath()
+                context2D.strokeStyle = borderColor.name
                 context2D.moveTo(fig.startX.toDouble(), fig.startY.toDouble())
                 context2D.lineTo(x.toDouble(), y.toDouble())
                 context2D.stroke()
@@ -171,16 +260,27 @@ class PaintCanvas(width: Int, height: Int) : Canvas(width, height) {
             }
             is Rectangle -> {
                 redraw()
+                context2D.strokeStyle = borderColor.name
                 context2D.strokeRect(
                     fig.startX.toDouble(),
                     fig.startY.toDouble(),
                     (x - fig.startX).toDouble(),
                     (y - fig.startY).toDouble()
                 )
+                if (fillColor != null) {
+                    context2D.fillStyle = fillColor?.name
+                    context2D.fillRect(
+                        fig.startX.toDouble(),
+                        fig.startY.toDouble(),
+                        (x - fig.startX).toDouble(),
+                        (y - fig.startY).toDouble()
+                    )
+                }
                 fig.endOfRect = Pair(x, y)
             }
             is Circle -> {
                 redraw()
+                context2D.strokeStyle = borderColor.name
                 context2D.beginPath()
                 context2D.ellipse(
                     fig.startX.toDouble(),
@@ -189,6 +289,10 @@ class PaintCanvas(width: Int, height: Int) : Canvas(width, height) {
                     abs((y - fig.startY).toDouble()), 0.0, 0.0, 2 * PI
                 )
                 context2D.stroke()
+                if (fillColor != null) {
+                    context2D.fillStyle = fillColor?.name
+                    context2D.fill()
+                }
                 fig.endOfCircle = Pair(x, y)
             }
         }
@@ -218,17 +322,19 @@ enum class Tool {
     CIRCLE
 }
 
-abstract class Fig {
+abstract class Fig(val startX: Int, val startY: Int, val borderColor: Col, val fillColor: Col?) {
     abstract fun draw(context: CanvasRenderingContext2D)
 }
 
-class Point(val x: Int, val y: Int) : Fig() {
+class Point(startX: Int, startY: Int, borderColor: Col) : Fig(startX, startY, borderColor, null) {
     override fun draw(context: CanvasRenderingContext2D) {
-        context.fillRect(x.toDouble(), y.toDouble(), 1.0, 1.0)
+        context.strokeStyle = borderColor.name
+        context.strokeRect(startX.toDouble(), startY.toDouble(), 1.0, 1.0)
     }
 }
 
-class Pencil(val startX: Int, val startY: Int) : Fig() {
+class Pencil(startX: Int, startY: Int, borderColor: Col) :
+    Fig(startX, startY, borderColor, null) {
     private val points = mutableListOf<Pair<Int, Int>>()
 
     fun addPoint(x: Int, y: Int) {
@@ -237,6 +343,7 @@ class Pencil(val startX: Int, val startY: Int) : Fig() {
 
     override fun draw(context: CanvasRenderingContext2D) {
         context.beginPath()
+        context.strokeStyle = borderColor.name
         context.moveTo(startX.toDouble(), startY.toDouble())
         points.forEach {
             context.lineTo(it.first.toDouble(), it.second.toDouble())
@@ -245,12 +352,13 @@ class Pencil(val startX: Int, val startY: Int) : Fig() {
     }
 }
 
-class Line(val startX: Int, val startY: Int) : Fig() {
+class Line(startX: Int, startY: Int, borderColor: Col) : Fig(startX, startY, borderColor, null) {
     var endOfLine: Pair<Int, Int>? = null
 
     override fun draw(context: CanvasRenderingContext2D) {
         endOfLine?.let {
             context.beginPath()
+            context.strokeStyle = borderColor.name
             context.moveTo(startX.toDouble(), startY.toDouble())
             context.lineTo(it.first.toDouble(), it.second.toDouble())
             context.stroke()
@@ -258,26 +366,39 @@ class Line(val startX: Int, val startY: Int) : Fig() {
     }
 }
 
-class Rectangle(val startX: Int, val startY: Int) : Fig() {
+class Rectangle(startX: Int, startY: Int, borderColor: Col, fillColor: Col?) :
+    Fig(startX, startY, borderColor, fillColor) {
     var endOfRect: Pair<Int, Int>? = null
 
     override fun draw(context: CanvasRenderingContext2D) {
         endOfRect?.let {
+            context.strokeStyle = borderColor.name
             context.strokeRect(
                 startX.toDouble(),
                 startY.toDouble(),
                 (it.first - startX).toDouble(),
                 (it.second - startY).toDouble()
             )
+            if (fillColor != null) {
+                context.fillStyle = fillColor.name
+                context.fillRect(
+                    startX.toDouble(),
+                    startY.toDouble(),
+                    (it.first - startX).toDouble(),
+                    (it.second - startY).toDouble()
+                )
+            }
         }
     }
 }
 
-class Circle(val startX: Int, val startY: Int) : Fig() {
+class Circle(startX: Int, startY: Int, borderColor: Col, fillColor: Col?) :
+    Fig(startX, startY, borderColor, fillColor) {
     var endOfCircle: Pair<Int, Int>? = null
 
     override fun draw(context: CanvasRenderingContext2D) {
         endOfCircle?.let {
+            context.strokeStyle = borderColor.name
             context.beginPath()
             context.ellipse(
                 startX.toDouble(),
@@ -286,6 +407,10 @@ class Circle(val startX: Int, val startY: Int) : Fig() {
                 abs((it.second - startY).toDouble()), 0.0, 0.0, 2 * PI
             )
             context.stroke()
+            if (fillColor != null) {
+                context.fillStyle = fillColor.name
+                context.fill()
+            }
         }
     }
 }
