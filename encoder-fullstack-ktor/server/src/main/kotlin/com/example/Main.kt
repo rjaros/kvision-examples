9f0744cb99a9bf21c16b277fa0f66e01c4c4f0fe
Robert Jaros <rjaros@finn.pl>
2019-03-05 22:18:47 +0100

    Example full-stack encoder application.

---
+++ encoder-fullstack-ktor/server/src/main/kotlin/com/example/Main.kt
@@ -0,0 +1,13 @@
+package com.example
+
+import io.ktor.application.Application
+import io.ktor.routing.routing
+import pl.treksoft.kvision.remote.applyRoutes
+import pl.treksoft.kvision.remote.kvisionInit
+
+fun Application.main() {
+    kvisionInit()
+    routing {
+        applyRoutes(EncodingServiceManager)
+    }
+}
