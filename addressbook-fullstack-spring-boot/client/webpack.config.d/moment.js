759e17fbd067f052ed2e1a65dd7e673fae69d40e
Robert Jaros <rjaros@finn.pl>
2019-05-14 11:02:01 +0200

    Example apps webpack configuration fixes.

--- addressbook-fullstack-spring-boot/client/webpack.config.d/moment.js
+++ addressbook-fullstack-spring-boot/client/webpack.config.d/moment.js
@@ -1,4 +1,9 @@
-config.plugins.push(new webpack.ProvidePlugin({
-    moment: "moment",
-    "window.moment": "moment"
-}));
+try {
+    const moment = require("moment");
+
+    config.plugins.push(new webpack.ProvidePlugin({
+	moment: "moment",
+	"window.moment": "moment"
+    }));
+} catch (e) {
+}
