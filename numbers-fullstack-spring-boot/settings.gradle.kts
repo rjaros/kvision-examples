adcac0a753e837e0474ac85902190c105c8dacca
Robert Jaros <rjaros@finn.pl>
2023-08-17 23:01:36 +0200

    Upgrade to KVision 7.0.0 - missing changes

--- numbers-fullstack-spring-boot/settings.gradle.kts
+++ numbers-fullstack-spring-boot/settings.gradle.kts
@@ -4,12 +4,5 @@ pluginManagement {
         mavenCentral()
         mavenLocal()
     }
-    resolutionStrategy {
-        eachPlugin {
-            when {
-                requested.id.id == "org.springframework.boot" -> useModule("org.springframework.boot:spring-boot-gradle-plugin:${requested.version}")
-            }
-        }
-    }
 }
 rootProject.name = "numbers-fullstack-spring-boot"
