a490cee034c570c0b3c526cca03fd2248be92747
Richard <richiethom@gmail.com>
2025-02-16 20:56:07 +0100

    Remove signup from FE

--- addressbook-fullstack-spring-boot-oauth/src/jsMain/kotlin/com/example/Security.kt
+++ addressbook-fullstack-spring-boot-oauth/src/jsMain/kotlin/com/example/Security.kt
@@ -14,6 +14,7 @@ import io.kvision.remote.Credentials
 import io.kvision.remote.LoginService
 import io.kvision.remote.SecurityMgr
 import io.kvision.utils.ENTER_KEY
+import kotlinx.browser.document
 import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 
@@ -27,121 +28,26 @@ actual data class Profile(
 
 class LoginWindow : Dialog<Credentials>(closeButton = false, escape = false, animation = false) {
 
-    private val loginPanel: FormPanel<Credentials>
     private val loginButton: Button
-    private val userButton: Button
-    private val registerPanel: FormPanel<Profile>
-    private val registerButton: Button
-    private val cancelButton: Button
 
     init {
-        loginPanel = formPanel {
-            add(Credentials::username, Text(label = "${tr("Login")}:"), required = true)
-            add(Credentials::password, Password(label = "${tr("Password")}:"), required = true)
-            onEvent {
-                keydown = {
-                    if (it.keyCode == ENTER_KEY) {
-                        this@LoginWindow.processCredentials()
-                    }
-                }
-            }
-        }
-        registerPanel = formPanel {
-            add(Profile::name, Text(label = "${tr("Your name")}:"), required = true)
-            add(Profile::username, Text(label = "Login:"), required = true)
-            add(
-                Profile::password, Password(label = "${tr("Password")}:"), required = true,
-                validatorMessage = { "Password too short" }) {
-                (it.getValue()?.length ?: 0) >= 8
-            }
-            add(Profile::password2, Password(label = "${tr("Confirm password")}:"), required = true,
-                validatorMessage = { tr("Password too short") }) {
-                (it.getValue()?.length ?: 0) >= 8
-            }
-            validator = {
-                val result = it[Profile::password] == it[Profile::password2]
-                if (!result) {
-                    it.getControl(Profile::password)?.validatorError = tr("Passwords are not the same")
-                    it.getControl(Profile::password2)?.validatorError = tr("Passwords are not the same")
-                }
-                result
-            }
-            validatorMessage = { tr("Passwords are not the same") }
-
-        }
-        cancelButton = Button(tr("Cancel"), "fas fa-times") {
-            onClick {
-                this@LoginWindow.hideRegisterForm()
-            }
-        }
-        registerButton = Button(tr("Register"), "fas fa-check", ButtonStyle.PRIMARY) {
-            onClick {
-                this@LoginWindow.processRegister()
-            }
-        }
         loginButton = Button(tr("Login"), "fas fa-check", ButtonStyle.PRIMARY) {
             onClick {
                 this@LoginWindow.processCredentials()
             }
         }
-        userButton = Button(tr("Register user"), "fas fa-user") {
-            onClick {
-                this@LoginWindow.showRegisterForm()
-            }
-        }
-        addButton(userButton)
         addButton(loginButton)
-        addButton(cancelButton)
-        addButton(registerButton)
-        hideRegisterForm()
-    }
-
-    private fun showRegisterForm() {
-        loginPanel.hide()
-        registerPanel.show()
-        registerPanel.clearData()
-        loginButton.hide()
-        userButton.hide()
-        cancelButton.show()
-        registerButton.show()
-    }
-
-    private fun hideRegisterForm() {
-        loginPanel.show()
-        registerPanel.hide()
-        loginButton.show()
-        userButton.show()
-        cancelButton.hide()
-        registerButton.hide()
     }
 
     private fun processCredentials() {
-        if (loginPanel.validate()) {
-            setResult(loginPanel.getData())
-            loginPanel.clearData()
-        }
+        document.location?.href = "/oauth2/authorization/google"
     }
 
-    private fun processRegister() {
-        if (registerPanel.validate()) {
-            val userData = registerPanel.getData()
-            AppScope.launch {
-                if (Model.registerProfile(userData, userData.password!!)
-                ) {
-                    Alert.show(text = tr("User registered. You can now log in.")) {
-                        hideRegisterForm()
-                    }
-                } else {
-                    Alert.show(text = tr("This login is not available. Please try again."))
-                }
-            }
-        }
-    }
 }
 
 object Security : SecurityMgr() {
 
-    private val loginService = LoginService("/login")
+    private val loginService = LoginService("/oauth2/authorization/google")
     private val loginWindow = LoginWindow()
 
     override suspend fun login(): Boolean {
