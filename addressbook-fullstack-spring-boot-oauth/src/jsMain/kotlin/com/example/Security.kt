5a2d0f29cf0874017df3fffecd9b31066c6b7103
Richard <richiethom@gmail.com>
2025-02-17 08:42:10 +0100

    Remove password from Profile

--- addressbook-fullstack-spring-boot-oauth/src/jsMain/kotlin/com/example/Security.kt
+++ addressbook-fullstack-spring-boot-oauth/src/jsMain/kotlin/com/example/Security.kt
@@ -1,29 +1,19 @@
 package com.example
 
-import io.kvision.core.onEvent
-import io.kvision.form.FormPanel
-import io.kvision.form.formPanel
-import io.kvision.form.text.Password
-import io.kvision.form.text.Text
 import io.kvision.html.Button
 import io.kvision.html.ButtonStyle
 import io.kvision.i18n.I18n.tr
-import io.kvision.modal.Alert
 import io.kvision.modal.Dialog
 import io.kvision.remote.Credentials
 import io.kvision.remote.LoginService
 import io.kvision.remote.SecurityMgr
-import io.kvision.utils.ENTER_KEY
 import kotlinx.browser.document
-import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 
 @Serializable
 actual data class Profile(
     val name: String? = null,
-    val username: String? = null,
-    val password: String? = null,
-    val password2: String? = null
+    val username: String? = null
 )
 
 class LoginWindow : Dialog<Credentials>(closeButton = false, escape = false, animation = false) {
